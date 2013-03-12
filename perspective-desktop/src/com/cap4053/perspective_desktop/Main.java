f74e8e38b955b03d5a3b5d29d9e33df8a91f16d6
Marc McDougall <marcjmcdougall@gmail.com>
2013-03-11 21:55:13 -0400

    Fixed simple package name bug

---
+++ perspective-desktop/src/com/cap4053/perspective_desktop/Main.java
@@ -0,0 +1,16 @@
+package com.cap4053.perspective_desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "perspective";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new com.cap4053.perspective.Main(), cfg);
+	}
+}
