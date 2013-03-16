6dbf6a1275999b6266a5944f191021facd44fba9
Marc McDougall <marcjmcdougall@gmail.com>
2013-03-16 18:09:54 -0400

    Refactoring name of Main class

---
+++ perspective/src/com/cap4053/perspective/Perspective.java
@@ -0,0 +1,46 @@
+package com.cap4053.perspective;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.cap4053.perspective.screens.GameScreen;
+import com.cap4053.perspective.view.PerspectiveInputProcessor;
+
+public class Perspective extends Game {
+	
+	@Override
+	public void create() {		
+		
+		setScreen(new GameScreen());
+		Gdx.input.setInputProcessor(new PerspectiveInputProcessor(this));
+	}
+	
+	@Override
+	public void dispose() {
+		
+		super.dispose();
+	}
+
+	@Override
+	public void render() {	
+		
+		super.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		
+		super.resume();
+	}
+}
