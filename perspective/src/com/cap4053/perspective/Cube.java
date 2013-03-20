f5b1a422856097905e79272591f86c8cfdce893c
Marc McDougall <marcjmcdougall@gmail.com>
2013-03-19 21:32:34 -0400

    Cube can now be textured.

--- perspective/src/com/cap4053/perspective/Cube.java
+++ perspective/src/com/cap4053/perspective/Cube.java
@@ -1,7 +1,6 @@
 package com.cap4053.perspective;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.Texture;
@@ -19,37 +18,37 @@ public class Cube {
 		// Note, there are 3 sets of vertices for each because we want to map the texture differently each time!
 		float vertices[] = {
 				 
-				 -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, // A (Top)	0
-				 -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, // A (Left)	1
-				 -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, // A (Back)	2
+				 -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, // A (Top)	0
+				 -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, // A (Left)	1
+				 -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, // A (Back)	2
 				 
-				 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,  // B (Top)	3
-				 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,  // B (Right)	4
-				 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,  // B (Back)	5
+				 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,  // B (Top)	3
+				 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,  // B (Right)	4
+				 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,  // B (Back)	5
 				 
-				 1.0f, 1.0f, -1.0f, 1.0f, 1.0f,   // C (Bottom)	6
-				 1.0f, 1.0f, -1.0f, 1.0f, 1.0f,   // C (Right)	7
-				 1.0f, 1.0f, -1.0f, 0.0f, 1.0f,   // C (Back)	8
+				 1.0f, 1.0f, -1.0f, 0.0f, 1.0f,   // C (Bottom)	6
+				 1.0f, 1.0f, -1.0f, 0.0f, 1.0f,   // C (Right)	7
+				 1.0f, 1.0f, -1.0f, 1.0f, 1.0f,   // C (Back)	8
 				 
-				 -1.0f, 1.0f, -1.0f, 0.0f, 1.0f,  // D (Bottom)	9
-				 -1.0f, 1.0f, -1.0f, 0.0f, 1.0f,  // D (Left)	10
-				 -1.0f, 1.0f, -1.0f, 1.0f, 1.0f,  // D (Back)	11
+				 -1.0f, 1.0f, -1.0f, 1.0f, 1.0f,  // D (Bottom)	9
+				 -1.0f, 1.0f, -1.0f, 1.0f, 1.0f,  // D (Left)	10
+				 -1.0f, 1.0f, -1.0f, 0.0f, 1.0f,  // D (Back)	11
 				 
-				 -1.0f, -1.0f, 1.0f, 0.0f, 1.0f,  // E (Top)	12
-				 -1.0f, -1.0f, 1.0f, 1.0f, 0.0f,  // E (Left)	13
-				 -1.0f, -1.0f, 1.0f, 0.0f, 0.0f,  // E (Front)	14
+				 -1.0f, -1.0f, 1.0f, 1.0f, 1.0f,  // E (Top)	12
+				 -1.0f, -1.0f, 1.0f, 0.0f, 0.0f,  // E (Left)	13
+				 -1.0f, -1.0f, 1.0f, 1.0f, 0.0f,  // E (Front)	14
 				 
-				 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,   // F (Top)	15
-				 1.0f, -1.0f, 1.0f, 0.0f, 0.0f,   // F (Right)	16
-				 1.0f, -1.0f, 1.0f, 1.0f, 0.0f,   // F (Front)	17
+				 1.0f, -1.0f, 1.0f, 0.0f, 1.0f,   // F (Top)	15
+				 1.0f, -1.0f, 1.0f, 1.0f, 0.0f,   // F (Right)	16
+				 1.0f, -1.0f, 1.0f, 0.0f, 0.0f,   // F (Front)	17
 				 
-				 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,    // G (Bottom)	18
-				 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,    // G (Right)	19
-				 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,    // G (Front)	20
+				 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,    // G (Bottom)	18
+				 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,    // G (Right)	19
+				 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,    // G (Front)	20
 				 
-				 -1.0f, 1.0f, 1.0f, 0.0f, 0.0f,   // H (Bottom)	21
-				 -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,   // H (Left)	22
-				 -1.0f, 1.0f, 1.0f, 0.0f, 1.0f    // H (Front)	23
+				 -1.0f, 1.0f, 1.0f, 1.0f, 0.0f,   // H (Bottom)	21
+				 -1.0f, 1.0f, 1.0f, 0.0f, 1.0f,   // H (Left)	22
+				 -1.0f, 1.0f, 1.0f, 1.0f, 1.0f    // H (Front)	23
 		 };
 		 
         // This is the index information.  This is where we tell OpenGL HOW to draw the vertexes on screen.
@@ -63,7 +62,7 @@ public class Cube {
         		19, 16, 4,		19, 4, 7,		// Right face
         		10, 1, 13,		10, 13, 22,		// Left face
         		12, 0, 15,		15, 0, 3,		// Top face
-        		9, 18, 21,		9, 6, 18		// Bottom face
+        		21, 18, 9,		18, 6, 9		// Bottom face
         };
         
         if(mesh == null){
