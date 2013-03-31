dd327c2b4f5fa627d0cba219994c32147bd1bca2
Marc McDougall <marcjmcdougall@gmail.com>
2013-03-31 18:43:07 -0400

    Removed public variables

--- perspective/src/com/cap4053/perspective/screens/GameScreen2D.java
+++ perspective/src/com/cap4053/perspective/screens/GameScreen2D.java
@@ -1,8 +1,5 @@
 package com.cap4053.perspective.screens;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -10,15 +7,13 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.cap4053.perspective.Perspective;
 import com.cap4053.perspective.backends.LevelManager;
 import com.cap4053.perspective.backends.Plane;
-import com.cap4053.perspective.models2D.PerspectiveItem;
 import com.cap4053.perspective.models2D.PerspectiveObject;
-import com.cap4053.perspective.models2D.tiles.Tile;
 import com.cap4053.perspective.view.GameInputProcessor2D;
 
 public class GameScreen2D extends PerspectiveScreen{
 
-	public static float HORIZONTAL_MARGIN = 20.0f;
-	public static float VERTICAL_MARGIN = 100.0f;
+	public static final float HORIZONTAL_MARGIN = 20.0f;
+	public static final float VERTICAL_MARGIN = 100.0f;
 	
 	private Plane level2D;
 	private LevelManager manager;
@@ -29,8 +24,6 @@ public class GameScreen2D extends PerspectiveScreen{
 		
 		this.manager = manager;
 		
-		updateScreenDimensions();
-			
 //		DEBUGS
 		Gdx.app.log(Perspective.TAG, "horizontal margin: " + HORIZONTAL_MARGIN + ", vertical margin: " + VERTICAL_MARGIN);
 		Gdx.app.log(Perspective.TAG, "width: " + Gdx.graphics.getWidth() + ", height: " + Gdx.graphics.getHeight());
@@ -57,17 +50,9 @@ public class GameScreen2D extends PerspectiveScreen{
 	
 	public void resize(int width, int height){
 		
-		updateScreenDimensions();
-		
 		super.resize(width, height);
 	}
 	
-	private void updateScreenDimensions(){
-		
-		HORIZONTAL_MARGIN = (Gdx.graphics.getWidth() - (7 * PerspectiveObject.SQUARE_DIMENSION)) / 2.0f;
-		VERTICAL_MARGIN = (Gdx.graphics.getHeight() - (7 * PerspectiveObject.SQUARE_DIMENSION)) / 2.0f;
-	}
-
 	/**
 	 * @return the level2D
 	 */
