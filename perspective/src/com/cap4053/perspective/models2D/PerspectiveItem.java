d5cfb0aeca8c5c13a53eb534f62cf2b7b9856512
Bryan Dubno <bryandubno@gmail.com>
2013-04-01 23:30:33 -0400

    Animation of item collection

--- perspective/src/com/cap4053/perspective/models2D/PerspectiveItem.java
+++ perspective/src/com/cap4053/perspective/models2D/PerspectiveItem.java
@@ -2,6 +2,9 @@ package com.cap4053.perspective.models2D;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.cap4053.perspective.Perspective;
 import com.cap4053.perspective.backends.Plane;
@@ -17,7 +20,10 @@ public abstract class PerspectiveItem extends PerspectiveObject {
 	
 	public void onMoveOver(Avatar collidedObject){
 		
-		remove();
+		//remove();
+		levitateItem();
+		
+		// Changes avatar to item
 		collidedObject.onPickUp(this);
 	}
 	
@@ -37,5 +43,17 @@ public abstract class PerspectiveItem extends PerspectiveObject {
 			
 			return super.remove();
 		}
+	}
+	
+	public void levitateItem(){
+		
+		this.addAction(Actions.moveTo(this.getX(), this.getY()+20, 0.35f, Interpolation.linear));
+		
+		SequenceAction sequence = new SequenceAction();
+		sequence.addAction(Actions.fadeOut(0.5f));
+		sequence.addAction(Actions.removeActor());
+		
+		this.addAction(sequence);
+		
 	}
 }
