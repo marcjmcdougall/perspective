450ebfcb80568023ce921a761b2c7252276f018e
Bryan Dubno <bryandubno@gmail.com>
2013-04-01 23:35:31 -0400

    Levitate items

--- perspective/src/com/cap4053/perspective/models2D/PerspectiveItem.java
+++ perspective/src/com/cap4053/perspective/models2D/PerspectiveItem.java
@@ -1,6 +1,9 @@
 package com.cap4053.perspective.models2D;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.cap4053.perspective.backends.Parser;
 import com.cap4053.perspective.backends.Plane;
@@ -20,7 +23,10 @@ public abstract class PerspectiveItem extends PerspectiveObject {
 	
 	public void onMoveOver(Avatar collidedObject){
 		
-		remove();
+		//remove();
+		levitateItem();
+		
+		// Changes avatar to item
 		collidedObject.onPickUp(this);
 	}
 	
@@ -41,5 +47,17 @@ public abstract class PerspectiveItem extends PerspectiveObject {
 			
 			return super.remove();
 		}
+	}
+	
+	public void levitateItem(){
+		
+		this.addAction(Actions.moveTo(this.getX(), this.getY()+20, 0.35f, Interpolation.linear));
+		
+		SequenceAction sequence = new SequenceAction();
+		sequence.addAction(Actions.fadeOut(0.5f));
+		sequence.addAction(Actions.removeActor());
+		
+		this.addAction(sequence);
+		
 	}
 }
