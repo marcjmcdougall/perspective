1387200fc204824e42a0ac6094e2aa852878b1aa
Bryan Dubno <bryandubno@gmail.com>
2013-04-16 17:21:51 -0400

    Modified collection item for summary screen

--- perspective/src/com/cap4053/perspective/models2D/PerspectiveCollectionItem.java
+++ perspective/src/com/cap4053/perspective/models2D/PerspectiveCollectionItem.java
@@ -22,7 +22,7 @@ public class PerspectiveCollectionItem extends Image{
 				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 			}
 		}
-		else{
+		else if(type.equalsIgnoreCase("Hearts")){
 			if(!enabled){
 				texture = new Texture(Gdx.files.internal("data/items/uncollected_heart.png"));
 				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
@@ -32,6 +32,27 @@ public class PerspectiveCollectionItem extends Image{
 				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 			}
 		}
+		else if(type.equalsIgnoreCase("Summary-Stars")){
+			if(!enabled){
+				texture = new Texture(Gdx.files.internal("data/items/summary-star-unfilled.png"));
+				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			}
+			else{
+				texture = new Texture(Gdx.files.internal("data/items/summary-star-filled.png"));
+				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			}
+		}
+		else if(type.equalsIgnoreCase("Summary-Hearts")){
+			if(!enabled){
+				texture = new Texture(Gdx.files.internal("data/items/summary-heart-unfilled.png"));
+				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			}
+			else{
+				texture = new Texture(Gdx.files.internal("data/items/summary-heart-filled.png"));
+				texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			}
+		}
+		
 		
 		return new PerspectiveCollectionItem(texture);
 	}
